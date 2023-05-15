8e4329e0aa45bce6ca8af8124ea0a86b9aecbb42
Vlad Shvets <shvets7200@gmail.com>
2023-05-15 16:46:36 +0500

    update

--- telegram_space_bot.py
+++ telegram_space_bot.py
@@ -5,10 +5,3 @@ def send_photo_to_tg(BOT_TOKEN, CHAT_ID, filename):
     bot = telegram.Bot(token=BOT_TOKEN)
     bot.send_photo(chat_id=CHAT_ID, photo=open(f'images/{filename}', 'rb'))
 
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
