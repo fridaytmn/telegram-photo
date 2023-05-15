65884761d9facaff3b6b3dbbfe1b2e86b32e4ad5
Vlad Shvets <shvets7200@gmail.com>
2023-05-15 13:44:12 +0500

    publish photo to Telegram channel.

--- telegram_space_bot.py
+++ telegram_space_bot.py
@@ -7,4 +7,4 @@ load_dotenv()
 BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 bot = telegram.Bot(token='6002224684:AAEoFSTjFYCRRRqgu3ampD0I_3iBax2_Vzk')
 print(bot.get_me())
-bot.send_message(chat_id='-1001514356106', text='test message')
+bot.send_photo(chat_id='-1001514356106', photo=open('images/SpaceX_0.jpg', 'rb'))
